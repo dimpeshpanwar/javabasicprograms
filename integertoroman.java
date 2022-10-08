68731096311504c33d53f68d9ac12fc5b6c55239
yuvihere23 <114753071+yuvihere23@users.noreply.github.com>
2022-10-08 12:47:52 +0530

    Update integertoroman.java

--- integertoroman.java
+++ integertoroman.java
@@ -1,9 +1,36 @@
-public static String intToRoman(int num) {
-    String M[] = {"", "M", "MM", "MMM"};
-    String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-    String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-    return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+// Java Program for above approach
+
+class  ItoR{
+	// Function to calculate roman equivalent
+	static String intToRoman(int num)
+	{
+		// storing roman values of digits from 0-9
+		// when placed at different places
+		String m[] = { "", "M", "MM", "MMM" };
+		String c[] = { "", "C", "CC", "CCC", "CD",
+					"D", "DC", "DCC", "DCCC", "CM" };
+		String x[] = { "", "X", "XX", "XXX", "XL",
+					"L", "LX", "LXX", "LXXX", "XC" };
+		String i[] = { "", "I", "II", "III", "IV",
+					"V", "VI", "VII", "VIII", "IX" };
+
+		// Converting to roman
+		String thousands = m[num / 1000];
+		String hundreds = c[(num % 1000) / 100];
+		String tens = x[(num % 100) / 10];
+		String ones = i[num % 10];
+
+		String ans = thousands + hundreds + tens + ones;
+
+		return ans;
+	}
+
+	// Driver program to test above function
+	public static void main(String[] args)
+	{
+		int number = 3549;
+		System.out.println(intToRoman(number));
+	}
 }
 
 Input : 9
