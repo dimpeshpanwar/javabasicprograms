046b6dc8170f647ca94e67d973cfb3eb92825673
Aditi Bansal <142652964+Aditi22Bansal@users.noreply.github.com>
2023-10-18 23:12:20 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class DiamondPattern {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows for the diamond (half of the diamond's height): ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
