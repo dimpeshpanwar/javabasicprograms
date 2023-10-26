c8ad815941d54369a604199855bbe2898ed5bf0a
PRIYANKA SHARMA <84637642+priyankashrama@users.noreply.github.com>
2023-10-26 14:21:45 +0530

    Create N_ball_Probability.java
    i have added new programs

---
+++ N_ball_Probability.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner scan=new Scanner(System.in);
+		int r,b,c;
+		r=scan.nextInt();
+		b=scan.nextInt();
+		c=scan.nextInt();
+		for(int i=r;i>=0;i--)
+		{
+		    int j=c-i;
+		    if(j>=0 && j<=b)
+		    {
+		        System.out.println(i+" "+j);
+		    }
+		}
+	}
+}
